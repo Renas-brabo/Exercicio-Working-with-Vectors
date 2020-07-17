0e1f4eb6aae1f789e175ade7bbc9764e2634223f
Renato <rrenatolg@gmail.com>
2020-07-17 16:54:13 -0300

    Working with vectors

--- working with vectors.R
+++ working with vectors.R
@@ -33,4 +33,26 @@ x <- 0
 
 
 
-exp((0.005 / 2000 / 200000 - 0.005) + 1) / exp (1 + 0.005)
+log((0.005 / (2000 / 200000 - 0.005)) + 1) / log (1 + 0.005)
+
+n <- c(2000,1800,1600,1400,1200,1000)
+
+log((0.005 / (1000 / 200000 - 0.005)) + 1) / log (1 + 0.005)
+
+# No, the last value does not surprise me. It's Infinite because the amount of months on the sample becomes too high.
+
+
+phi.paris <- 48.8566
+phi.ny <- 40.7128
+lambda.paris <- 2.3522
+lambda.ny <- -74.0060
+
+radians1 = phi.paris * (pi / 180)
+radians2 = phi.ny * (pi / 180)
+radians3 = lambda.paris * (pi / 180)
+radians4 = lambda.ny * (pi / 180)
+
+latitude = acos(sin(radians1)*sin(radians2) + cos(radians3)*cos(radians4)*cos(radians4-radians3))
+distance = latitude * 6371
+distance
+
